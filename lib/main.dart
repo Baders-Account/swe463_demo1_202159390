bf84c0fc17215f982fa9b5bd498936081b49b7a9
Bader <bdr-mtr@outlook.com>
2026-08-25 14:19:55 +0300

    Checkpoint 6 is Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp( const MyApp()
+  runApp( MaterialApp(
+    home: HomePage(),
+  ),
 
     // Checkpoint 1
 
@@ -79,60 +81,99 @@ void main() {
 
 }
 
-// checkpoint 4 till 6 
-class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-    @override
-    Widget build(BuildContext context){
-      return // Checkpoint 4
-    //  MaterialApp(
-    //   debugShowCheckedModeBanner: false,
-    //   home: Scaffold(
-    //     appBar: AppBar(
-    //       title: const Text('SWE 463 - Demo 1'),
-    //       backgroundColor: Colors.indigo,
-    //       foregroundColor: Colors.white,
-    //     ),
+// checkpoint 4 and 5 
+// class MyApp extends StatelessWidget {
+//     const MyApp({super.key});
+//     @override
+//     Widget build(BuildContext context){
+//       return // Checkpoint 4
+//     //  MaterialApp(
+//     //   debugShowCheckedModeBanner: false,
+//     //   home: Scaffold(
+//     //     appBar: AppBar(
+//     //       title: const Text('SWE 463 - Demo 1'),
+//     //       backgroundColor: Colors.indigo,
+//     //       foregroundColor: Colors.white,
+//     //     ),
         
-    //     drawer: const Drawer(
-    //       child: SafeArea(
-    //         child: Column(
-    //           children: [
-    //             DrawerHeader(
-    //               child: Center(
-    //                 child: Text("Demo Menu",style: TextStyle(fontSize: 24)
-    //                 )
-    //               )
-    //             ),
-    //             ListTile(
-    //               leading: Icon(Icons.home),
-    //               title: Text('Home'),
-    //             )
-    //           ]
-    //         )
-    //       )
+//     //     drawer: const Drawer(
+//     //       child: SafeArea(
+//     //         child: Column(
+//     //           children: [
+//     //             DrawerHeader(
+//     //               child: Center(
+//     //                 child: Text("Demo Menu",style: TextStyle(fontSize: 24)
+//     //                 )
+//     //               )
+//     //             ),
+//     //             ListTile(
+//     //               leading: Icon(Icons.home),
+//     //               title: Text('Home'),
+//     //             )
+//     //           ]
+//     //         )
+//     //       )
           
-    //     ),
+//     //     ),
 
-    //     body: const Center(
-    //       child: Text('Hot Reload is working!', style: TextStyle(fontSize: 24) )
-    //     )
-
-
-    //   )
-
-    // );
+//     //     body: const Center(
+//     //       child: Text('Hot Reload is working!', style: TextStyle(fontSize: 24) )
+//     //     )
 
 
-    // Checkpoint 5
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SWE 463 Demo 1',
-      home: const HomePage(),
-    );
-    }
-  }
+//     //   )
 
+//     // );
+
+
+//     // Checkpoint 5
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'SWE 463 Demo 1',
+//       home: const HomePage(),
+//     );
+//     }
+//   }
+
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override 
+//   Widget build(BuildContext context){
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Home Page'),
+//         ),
+//         body: const Center(
+//           child: Text('This the Home Page', style: TextStyle(fontSize:24)),
+//         )
+//       );
+//   }
+// }
+
+
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
+//   @override
+
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Login Page'),
+//       ),
+//       body: const Center(
+//         child: Text('This is the Login Page', style: TextStyle(fontSize:24)),
+//       ),
+//     );
+//   }
+
+// }
+
+
+
+
+// Checkpoint 6
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -140,15 +181,23 @@ class HomePage extends StatelessWidget {
   @override 
   Widget build(BuildContext context){
       return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: const Center(
-          child: Text('This the Home Page', style: TextStyle(fontSize:24)),
-        )
+       body:  Center(
+          child: ElevatedButton(
+            child: Text('Go to Login'),
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) => LoginPage(),
+              ));
+            }
+          )
+       )
       );
   }
 }
+
+
+
+
 
 
 class LoginPage extends StatelessWidget {
@@ -157,13 +206,16 @@ class LoginPage extends StatelessWidget {
 
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: const Center(
-        child: Text('This is the Login Page', style: TextStyle(fontSize:24)),
-      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Go to Home"),
+          onPressed:() {
+            Navigator.pop(context, MaterialPageRoute(
+              builder: (context) => HomePage(),
+            ));
+          }
+        )
+      )
     );
   }
-
 }
