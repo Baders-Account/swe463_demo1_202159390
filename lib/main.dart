5bf815f67ba2f3ee4644aee5bea94aabb311f066
Bader <bdr-mtr@outlook.com>
2026-08-25 12:59:34 +0300

    CheckPoint 2 is Done

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import "package:flutter/material.dart";
 
 void main() {
   runApp(
+    // const MaterialApp(
+    //   home: Text('My name is Bader Almutairi'),
+    // ),
     const MaterialApp(
-      home: Text('My name is Bader Almutairi'),
-    ),
+      debugShowCheckedModeBanner: false,      
+      home: Scaffold(
+          body: Center(
+            child: Text("Welcome to SWE 463", style: TextStyle(fontSize: 24)),
+          )
+
+
+      )
+    )
 
   );
 }
