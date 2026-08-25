1a91b6d2bafcc149495c1d03cdf5d2e2bb530742
Bader <bdr-mtr@outlook.com>
2026-08-25 14:01:47 +0300

    Checkpoint 5 is Done

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,48 @@ void main() {
     // )
 
 
-    
+    // Checkpoint 3
 
-    // Checkpoint 4
+
+    //      MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('SWE 463 - Demo 1'),
+    //       backgroundColor: Colors.indigo,
+    //       foregroundColor: Colors.white,
+    //     ),
+        
+    //     drawer: const Drawer(
+    //       child: SafeArea(
+    //         child: Column(
+    //           children: [
+    //             DrawerHeader(
+    //               child: Center(
+    //                 child: Text("Demo Menu",style: TextStyle(fontSize: 24)
+    //                 )
+    //               )
+    //             ),
+    //             ListTile(
+    //               leading: Icon(Icons.home),
+    //               title: Text('Home'),
+    //             )
+    //           ]
+    //         )
+    //       )
+          
+    //     ),
+
+    //     body: const Center(
+    //       child: Text('Open the drawer from the menu icon', style: TextStyle(fontSize: 24) )
+    //     )
+
+
+    //   )
+
+    // );
+
+  
 
 
 
@@ -40,48 +79,91 @@ void main() {
 
 }
 
-// Checkpoint 5
+// checkpoint 4 till 6 
 class MyApp extends StatelessWidget {
     const MyApp({super.key});
     @override
     Widget build(BuildContext context){
-      return // Checkpoint 3
-     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SWE 463 - Demo 1'),
-          backgroundColor: Colors.indigo,
-          foregroundColor: Colors.white,
-        ),
+      return // Checkpoint 4
+    //  MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('SWE 463 - Demo 1'),
+    //       backgroundColor: Colors.indigo,
+    //       foregroundColor: Colors.white,
+    //     ),
         
-        drawer: const Drawer(
-          child: SafeArea(
-            child: Column(
-              children: [
-                DrawerHeader(
-                  child: Center(
-                    child: Text("Demo Menu",style: TextStyle(fontSize: 24)
-                    )
-                  )
-                ),
-                ListTile(
-                  leading: Icon(Icons.home),
-                  title: Text('Home'),
-                )
-              ]
-            )
-          )
+    //     drawer: const Drawer(
+    //       child: SafeArea(
+    //         child: Column(
+    //           children: [
+    //             DrawerHeader(
+    //               child: Center(
+    //                 child: Text("Demo Menu",style: TextStyle(fontSize: 24)
+    //                 )
+    //               )
+    //             ),
+    //             ListTile(
+    //               leading: Icon(Icons.home),
+    //               title: Text('Home'),
+    //             )
+    //           ]
+    //         )
+    //       )
           
-        ),
+    //     ),
 
-        body: const Center(
-          child: Text('Hot Reload is working!', style: TextStyle(fontSize: 24) )
-        )
+    //     body: const Center(
+    //       child: Text('Hot Reload is working!', style: TextStyle(fontSize: 24) )
+    //     )
 
 
-      )
+    //   )
 
+    // );
+
+
+    // Checkpoint 5
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SWE 463 Demo 1',
+      home: const HomePage(),
     );
     }
   }
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: const Center(
+          child: Text('This the Home Page', style: TextStyle(fontSize:24)),
+        )
+      );
+  }
+}
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: const Center(
+        child: Text('This is the Login Page', style: TextStyle(fontSize:24)),
+      ),
+    );
+  }
+
+}
